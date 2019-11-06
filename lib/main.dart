3d63318b196a432338926748785412f6b6902ec7
maciej.wiatrowski <maciej.wiatrowski@primigenius.pl>
2019-11-06 13:43:01 +0100

    Change name

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:camera/camera.dart';
-import 'package:camera_app/display.dart';
+import 'package:primigenius_camera_app/display.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart' show join;
 import 'package:path_provider/path_provider.dart';
